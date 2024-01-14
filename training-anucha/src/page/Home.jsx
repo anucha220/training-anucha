b4db4cf7ee319cbf982934a60d8f614ec373e613
anucha220 <aanucha220@gmail.com>
2024-01-14 18:03:40 +0700

    first commit

---
+++ training-anucha/src/page/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+    <h1>หน้าหลัก</h1>
+      <hr />
+      <img src="123.jpg" alt="" />
+    </div>
+  )
+}
+
+export default Contact
